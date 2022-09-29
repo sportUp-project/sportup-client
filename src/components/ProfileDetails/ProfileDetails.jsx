ad8fc5cf3091f25f7a1b3d05aebe91780e77001b
Pavel Popov <pcveykos@gmail.com>
2022-09-29 16:03:00 +0200

    Add working profile editing page and form.

---
+++ src/components/ProfileDetails/ProfileDetails.jsx
@@ -0,0 +1,15 @@
+import "./ProfileDetails.css";
+import {Link} from 'react-router-dom'
+
+export default function ProfileDetails(props) {
+  const { userInfo } = props;
+  return (
+    <div className="profile-holder">
+      <h4>{userInfo.name}</h4>
+
+      <h4>Sports: {userInfo.sports}</h4>
+      <h4>Joined Activites:{userInfo.joinedActivities}</h4>
+      <h4>User created activities: {userInfo.userActivities}</h4>
+    </div>
+  );
+}
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect, useContext } from "react";
+import { Link, Route, Routes, useNavigate, useParams } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
+import ProfileDetails from '../components/ProfileDetails/ProfileDetails'
+import ProfileEditPage from "./ProfileEditPage";
+import axios from "axios";
+
+export default function ProfilePage(props) {
+  const { user } = useContext(AuthContext);
+  const { id: pageUserId } = useParams();
+
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    // get the token from localStorage
+    const storedToken = localStorage.getItem("authToken");
+    // and send it as part of the request to see the profile page
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/profile/${pageUserId}`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => {
+        setUserInfo(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log({userInfo})
+  console.log({user})
+  if (userInfo === null) {
+    return <span>Loading</span>;
+  }
+
+  return (
+    <div className="profile-page">
+      <ProfileDetails userInfo={userInfo} />
+      {user._id === userInfo._id && <Link to={`/profile/${user._id}/edit`}>Edit profile </Link>}
+    </div>
+
+  )
+}
