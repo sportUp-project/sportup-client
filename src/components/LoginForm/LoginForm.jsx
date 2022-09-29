278a9855821a00cee1b108a6ef7de08a6462a64d
Pavel Popov <pcveykos@gmail.com>
2022-09-29 11:47:51 +0200

    Add basic authentication pages and components

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,59 @@
+import { useState, useContext } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/auth.context";
+import './LoginForm.css'
+
+export default function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const navigate = useNavigate();
+  const { storeToken, authenticateUser } = useContext(AuthContext);
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { email, password };
+
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/auth/login`, requestBody)
+      .then((response) => {
+        // Request to the server's endpoint `/auth/login` returns a response
+        // with the JWT string ->  response.data.authToken
+        // console.log('JWT token', response.data.authToken );
+        storeToken(response.data.authToken); // store in my localStorage the authToken
+        authenticateUser(); // verify token is valid to get the user information from the server and store in state
+        navigate("/"); // <== ADD
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
+
+  return (
+    <form className="login-form" onSubmit={handleLoginSubmit}>
+      <label htmlFor="email">Email:</label>
+      <input
+        type="text"
+        name="email"
+        id="email"
+        value={email}
+        onChange={handleEmail}
+      />
+
+      <label forHtml="password">Password:</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        value={password}
+        onChange={handlePassword}
+      />
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      <button type="submit">Login</button>
+    </form>
+  );
+}
---
+++ src/components/SignupForm/SignupForm.jsx
@@ -0,0 +1,61 @@
+import './SignupForm.css'
+
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+
+export default function Signup(props) {
+  const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('');
+  const [errorMessage,setErrorMessage] = useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const requestToServer = {name,email, password, }
+
+    axios.post(process.env.REACT_APP_API_URL + '/auth/signup', requestToServer)
+    .then(response => {
+      navigate('/login')
+    })
+    .catch(err => {
+      const errorDescription = err.response.data.message;
+      setErrorMessage(errorDescription)
+    })
+  }
+
+  return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          id="name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor='email'>Email</label>
+        <input
+          type="text"
+          name="name"
+          value={email}
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+          />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="campus">Campus</label>
+        <button type='submit'>Sign up</button>
+      { errorMessage && <p className="error-message">{errorMessage}</p> }
+      </form>
+
+  );
+}
