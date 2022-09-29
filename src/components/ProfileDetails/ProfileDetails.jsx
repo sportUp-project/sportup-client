410390f74079f92dc18a3e486853a2d4c68890dd
Pavel Popov <pcveykos@gmail.com>
2022-09-29 16:09:46 +0200

    Add fixes on editing profile

--- src/components/ProfileDetails/ProfileDetails.jsx
+++ src/components/ProfileDetails/ProfileDetails.jsx
@@ -6,7 +6,8 @@ export default function ProfileDetails(props) {
   return (
     <div className="profile-holder">
       <h4>{userInfo.name}</h4>
-
+      <img src={userInfo.image} alt="profile" />
+      <h4>Description: {userInfo.description}</h4>
       <h4>Sports: {userInfo.sports}</h4>
       <h4>Joined Activites:{userInfo.joinedActivities}</h4>
       <h4>User created activities: {userInfo.userActivities}</h4>
