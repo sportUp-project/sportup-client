81098485a5aa666f39b195f4b2c301c77f5b1ab8
Joanna Zielinska <joannazielinskam@gmail.com>
2022-09-30 17:20:31 +0200

    Search bar component and SportMain

--- src/pages/SportMainPage.jsx
+++ src/pages/SportMainPage.jsx
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
-
-import ActivitiesList from '../components/ActivitiesList/ActivitiesList'
+//import ActivityCard from '../components/ActivityCard/ActivityCard';
+//import ActivitiesList from '../components/ActivitiesList/ActivitiesList';
+//import Search from '../components/Search/Search';
 
 
 function SportMainPage(props) {
     const {id} = useParams()
     const [sport,setSport] = useState(null)
 
+   // const [activities, setactivities] = useState([])
+   
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_API_URL}/api/sports/${id}`)
             .then(response => setSport(response.data))
             .catch(err => console.log(err))
     }, [])
+
+    // useEffect(() => {
+       
 
     if (sport === null) {
         return <span>Loading...</span>
@@ -25,9 +31,9 @@ function SportMainPage(props) {
         <img src={sport.imageUrl} alt={ `background ${sport.name}`} className='background-img sport-img'  />
         <h3>{sport.name}</h3>
         <span>{sport.activities.length} activities</span>
-        <h3>Latest added activities</h3>
-        <ActivitiesList activities={sport.activities} />       
-        
+   
+        {/* <Search activities={activities} /> */}
+
       </div>
     )
   
