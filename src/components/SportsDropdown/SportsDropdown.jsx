a32b9c07be486eeb301f0d97be4c49711738b710
Pavel Popov <pcveykos@gmail.com>
2022-09-30 11:30:37 +0200

    Change SportsDropdown to a form for simpler functioning

--- src/components/SportsDropdown/SportsDropdown.jsx
+++ src/components/SportsDropdown/SportsDropdown.jsx
@@ -1,24 +1,29 @@
 import './SportsDropdown.css'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export default function SportsDropdown(props) {
   const {sports} = props
+  const [chosenSport,setChosenSport] = useState('')
+
   const navigate = useNavigate()
   function handleChange(e) {
-    navigate(e.target.value)
-  }
-  function handleClick(e) {
-    console.log('clicked:' + e.target.value)
+    e.preventDefault();
+    navigate(chosenSport)
   }
 
+  // later fix issue with already preselected sport not working
   return (
-    <select name='dropdown' id='dropdown' onChange={handleChange}>
-      <option disabled selected value> -- select a sport -- </option>
-      {sports.map(sport => {
-        return (
-          <option value={`sport/${sport._id}`}>{sport.name} : ({sports.activities?.length})</option>
-        )
-      })}
-    </select>
+    <form onSubmit={handleChange}>
+      <select name='dropdown' id='dropdown' onChange={e => setChosenSport(e.target.value)}>
+        <option disabled selected value> -- select a sport -- </option>
+        {sports.map(sport => {
+          return (
+            <option value={`sport/${sport._id}`}>{sport.name} : ({sports.activities?.length})</option>
+          )
+        })}
+      </select>
+      <button type='submit'>Go to sport</button>
+    </form> 
   )
 }
