697d8ce1d04bdb920b223e422b8a8c21212856f5
Pavel Popov <pcveykos@gmail.com>
2022-09-30 11:16:23 +0200

    Add basic dropdown functionality

--- src/components/SportsDropdown/SportsDropdown.jsx
+++ src/components/SportsDropdown/SportsDropdown.jsx
@@ -1,2 +1,19 @@
 import './SportsDropdown.css'
+import { useNavigate } from 'react-router-dom'
 
+export default function SportsDropdown(props) {
+  const {sports} = props
+  const navigate = useNavigate()
+  function handleChange(e) {
+    navigate(e.target.value)
+  }
+  return (
+    <select name='dropdown' id='dropdown' onChange={handleChange}>
+      {sports.map(sport => {
+        return (
+          <option value={`sport/${sport._id}`} onSelect>{sport.name}</option>
+        )
+      })}
+    </select>
+  )
+}
