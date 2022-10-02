490d63295dbf0c652dd8f345e1bc76e9a996e249
Joanna Zielinska <joannazielinskam@gmail.com>
2022-10-02 13:55:57 +0200

    adjuested user profile page2

--- src/components/ProfileDetails/ProfileDetails.jsx
+++ src/components/ProfileDetails/ProfileDetails.jsx
@@ -9,7 +9,14 @@ export default function ProfileDetails(props) {
       <h4>{userInfo.name}</h4>
       <img src={userInfo.image} alt="profile" />
       <h4>Description: {userInfo.description}</h4>
-      <h4>Sports: {userInfo.sports}</h4>
+      <h4>Sports: 
+      {userInfo.sports.map((sport)=> {
+        return <div key={sport._id}>
+          <img src={sport.iconUrl} alt={sport.name} />
+          <p>{sport.name}</p>
+        </div>
+      })}
+      </h4>
       <h4>Joined Activites:
       {userInfo.joinedActivities.map((activity)=> {
         return <p key={activity._id}>{activity.name}</p>
