3b560f1f962343e6fc8230c5251ad135cab31ac1
Pavel Popov <pcveykos@gmail.com>
2022-10-02 12:05:40 +0200

    Import AuthContext to create conditional button for joining/leaving an event

--- src/pages/ActivityDetails.jsx
+++ src/pages/ActivityDetails.jsx
@@ -1,17 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Map, Marker } from "react-map-gl";
 import moment from "moment";
+import { AuthContext } from "../context/auth.context";
 
 export default function ActivityDetails(props) {
   const { id } = useParams();
   const [activity, setActivity] = useState(null);
   const storedToken = localStorage.getItem("authToken");
   const navigate = useNavigate();
+  const {user} = useContext(AuthContext)
+  console.log(user)
 
-  // map states
-
+  // fetch the data from server
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_API_URL}/api/activities/${id}`, {
@@ -26,6 +28,7 @@ export default function ActivityDetails(props) {
       });
   }, []);
 
+  // define the options for the map, later call it conditionally on load
   function renderMap() {
     return (
       <div className="map-holder">
@@ -47,15 +50,32 @@ export default function ActivityDetails(props) {
     );
   }
 
+  // create a formatted date with momentjs
   function renderDate() {
     return moment(activity.activityDate).format("MMMM do YYYY, hh:mm");
   }
-
   const dateFormatted = activity ? renderDate() : null;
 
+
+  function handleJoin(e) {
+    e.preventDefault()
+    axios.get(`${process.env.REACT_APP_API_URL}/api/activities/${activity._id}/join`,
+    {headers: { Authorization: `Bearer ${storedToken}` }})
+    .then(response => {
+      setActivity(response.data)
+    })
+    .catch(err => console.log(err))
+  }
+
+  
+
+  
+
+  //show loading while fetching the data
   if (activity === null) {
     return <span>Loading...</span>;
   }
+
 
   console.log(activity);
   return (
@@ -73,11 +93,13 @@ export default function ActivityDetails(props) {
       <p>{dateFormatted}</p>
       <p>Duration: {activity.duration} hours</p>
       <p>Members joining:</p>
+      {activity.createdBy._id !== user._id && <button onClick={handleJoin}>Join this activity</button>}
       {activity.members.map((member) => {
-        return <p>{member.name}</p>;
+        return <p key={member._id}>{member.name}</p>;
       })}
 
       
+
       {/* conditionally render map to avoid errors */}
       {activity !== null && renderMap()}
     </div>
