857651d3022a691423ff679292055af13a3ed8c8
Pavel Popov <pcveykos@gmail.com>
2022-10-02 09:43:03 +0200

    Add map to activity details page

--- src/pages/ActivityDetails.jsx
+++ src/pages/ActivityDetails.jsx
@@ -1,22 +1,61 @@
 import {useState, useEffect} from 'react'
-import {Link, useParams} from 'react-router-dom'
+import {Link, useParams, useNavigate} from 'react-router-dom'
 import axios from 'axios'
+import {Map, Marker} from 'react-map-gl'
 
 export default function ActivityDetails(props) {
   const {id} = useParams()
   const [activity, setActivity] = useState(null)
   const storedToken = localStorage.getItem('authToken')
+  const navigate = useNavigate()
+  const [viewState, setViewState] = useState({
 
-  useEffect(() => {
-    axios.get(
-      `${process.env.REACT_APP_API_URL}/api/activities/${id}`,
+  });
+
+
+    // map states
+    
+    
+    useEffect(() => {
+      axios.get(
+        `${process.env.REACT_APP_API_URL}/api/activities/${id}`,
       {headers: {Authorization: `Bearer ${storedToken}`}})
       .then(response => {
         setActivity(response.data)
       })
-      .catch(err => console.log(err))
-  }, [])
+      .catch(err => {
+        // navigate('/login')
+        console.log(err)
+      })
+    }, [])
   
+  function renderMap() {
+    return (
+
+    
+    <div className="map-holder">
+        <Map
+        initialViewState={{
+        longitude: activity.location.long,
+        latitude: activity.location.lat,
+        zoom: 12
+      }}
+      style={{ height: 400, width: 400 }}
+      onMove={(evt) => setViewState(evt.viewState)}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+
+    >
+      <Marker
+        longitude={activity.location.long}
+        latitude={activity.location.lat}
+      ></Marker>
+
+    </Map>
+      </div>
+      )
+  }
+
+
   if (activity === null) {
     return <span>Loading...</span>
   }
@@ -24,8 +63,14 @@ export default function ActivityDetails(props) {
   return (
     <div className="activity-details">
       <h4>{activity.name}</h4>
-      <h4>{activity.createdBy.name}</h4>
-      <h4>{activity.sport?.name}</h4>
+      <p>{activity.createdBy.name}</p>
+      <p>{activity.sport?.name}</p>
+      <p></p>
+
+
+      {/* conditionally render map to avoid errors */}
+      {activity !== null && renderMap()}
+
     </div>
   )
 }
