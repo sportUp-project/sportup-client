78ed97ad6249b973c1e276ba9d29336766203f54
Pavel Popov <pcveykos@gmail.com>
2022-10-02 11:32:49 +0200

    Add momentjs to project and display formatted date in activity details

--- src/pages/ActivityDetails.jsx
+++ src/pages/ActivityDetails.jsx
@@ -1,78 +1,85 @@
-import {useState, useEffect} from 'react'
-import {Link, useParams, useNavigate} from 'react-router-dom'
-import axios from 'axios'
-import {Map, Marker} from 'react-map-gl'
+import { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Map, Marker } from "react-map-gl";
+import moment from "moment";
 
 export default function ActivityDetails(props) {
-  const {id} = useParams()
-  const [activity, setActivity] = useState(null)
-  const storedToken = localStorage.getItem('authToken')
-  const navigate = useNavigate()
+  const { id } = useParams();
+  const [activity, setActivity] = useState(null);
+  const storedToken = localStorage.getItem("authToken");
+  const navigate = useNavigate();
 
+  // map states
 
-    // map states
-    
-    
-    useEffect(() => {
-      axios.get(
-        `${process.env.REACT_APP_API_URL}/api/activities/${id}`,
-      {headers: {Authorization: `Bearer ${storedToken}`}})
-      .then(response => {
-        setActivity(response.data)
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/api/activities/${id}`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
       })
-      .catch(err => {
+      .then((response) => {
+        setActivity(response.data);
+      })
+      .catch((err) => {
         // navigate('/login')
-        console.log(err)
-      })
-    }, [])
-  
+        console.log(err);
+      });
+  }, []);
+
   function renderMap() {
     return (
-
-    
-    <div className="map-holder">
+      <div className="map-holder">
         <Map
-        initialViewState={{
-        longitude: activity.location.long,
-        latitude: activity.location.lat,
-        zoom: 12
-      }}
-      style={{ height: 300, width: 300 }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-
-    >
-      <Marker
-        longitude={activity.location.long}
-        latitude={activity.location.lat}
-      ></Marker>
-
-    </Map>
+          initialViewState={{
+            longitude: activity.location.long,
+            latitude: activity.location.lat,
+            zoom: 12,
+          }}
+          style={{ height: 300, width: 300 }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+        >
+          <Marker
+            longitude={activity.location.long}
+            latitude={activity.location.lat}
+          ></Marker>
+        </Map>
       </div>
-      )
+    );
   }
 
+  function renderDate() {
+    return moment(activity.activityDate).format("MMMM do YYYY, hh:mm");
+  }
+
+  const dateFormatted = activity ? renderDate() : null;
 
   if (activity === null) {
-    return <span>Loading...</span>
+    return <span>Loading...</span>;
   }
-  console.log(activity)
+
+  console.log(activity);
   return (
     <div className="activity-details">
       <h4>{activity.name}</h4>
-      <Link to={`/profile/${activity.createdBy._id}`}><p>{activity.createdBy.name}</p></Link>
-      <p><Link to={`/activities/sport/${activity.sport._id}`}>{activity.sport?.name}</Link></p>
+      <Link to={`/profile/${activity.createdBy._id}`}>
+        <p>{activity.createdBy.name}</p>
+      </Link>
+      <p>
+        <Link to={`/activities/sport/${activity.sport._id}`}>
+          {activity.sport?.name}
+        </Link>
+      </p>
       <p>{activity.description}</p>
-      <p>{activity.activityDate}</p>
+      <p>{dateFormatted}</p>
       <p>Duration: {activity.duration} hours</p>
       <p>Members joining:</p>
-      {activity.members.map(member => {
-        return <p>{member.name}</p>
+      {activity.members.map((member) => {
+        return <p>{member.name}</p>;
       })}
 
-
+      
       {/* conditionally render map to avoid errors */}
       {activity !== null && renderMap()}
-
     </div>
-  )
+  );
 }
