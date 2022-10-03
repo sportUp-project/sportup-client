4a8c62d666ec7f8de6d9ec4e81d7c806283d2286
Pavel Popov <pcveykos@gmail.com>
2022-10-03 21:20:12 +0200

    Create basics of UserCard component

---
+++ src/components/UserCard/UserCard.jsx
@@ -0,0 +1,14 @@
+import './UserCard.css'
+
+
+export default function UserCard(props) {
+  const {user} = props;
+
+  return (
+    <div className="user-card">
+      <img src={user.image} alt={user.name} />
+      <span>{user.name}</span>
+      
+    </div>
+  )
+}
