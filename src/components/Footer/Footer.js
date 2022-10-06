f160cf90951b17c07e0206d1ecf734ac67ef9824
Pavel Popov <pcveykos@gmail.com>
2022-10-06 15:25:42 +0200

    Add changes to sport choosing at editing profiles.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,17 @@
-import './Footer.css'
-import { Link } from 'react-router-dom'
+import "./Footer.css";
+import { Link } from "react-router-dom";
 
-export default function Footer(props){
-    return(
-        <footer className='footer'>
-            <div className='footer-logo'>
-                <h1><span>Sport</span>UP</h1>
-            </div>
-            
-            <h3>Created by:</h3>
-            <ul>
-                <li><a href='https://github.com/j-zielinska'>Janna Zielinska </a></li>
-                <li><a href='https://github.com/veykos'>Pavel Popov</a></li>
-            </ul>                
-            
-        </footer>
-    )
+export default function Footer(props) {
+  return (
+    <footer className="footer">
+      <div className="footer-logo">
+        <h1>
+          <span>Sport</span>UP
+        </h1>
+      </div>
+      <Link to={"/about"}>
+        <span>About us</span>
+      </Link>
+    </footer>
+  );
 }
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,23 +4,23 @@ export default function AboutPage(props) {
   return (
     <div className="about-holder">
       <section className="creator-holder">
-        <h3>Pavel Popov</h3>
-        <img src='https://res.cloudinary.com/dxc2oj7lt/image/upload/v1664887616/sportUp-users-gallery/gxzye5vhjmvkus7kn7mu.jpg' alt="" />
-        <p>Hi guys this is my description Lorem ipsum dolor sit amet consectetur adipisicing elit. In tempora officia possimus fuga, itaque ullam rerum dolorem quo deleniti! Dolores tempore perspiciatis voluptate sed quia sint sunt, porro ipsa qui.</p>
-        <div className="sc-holder">
-
-        <a rel='noreferrer' target="_blank" href="https://www.linkedin.com/in/pavel-iv-popov/"><img className='link-logo' src="https://res.cloudinary.com/dxc2oj7lt/image/upload/v1665059333/sportUp-users-gallery/linkedin_tscs52.png" alt="github" /></a>
-        <a rel='noreferrer' target="_blank" href="https://github.com/veykos"><img className='link-logo' src="https://res.cloudinary.com/dxc2oj7lt/image/upload/v1665058862/sportUp-users-gallery/github-logo_kyaxov.png" alt="github" /></a>
-        </div>
-      </section>
-      <section className="creator-holder">
         <h3>Joanna Zielinska</h3>
         <img src="" alt="" />
-        <p>Hi guys this is my description Lorem ipsum dolor sit amet consectetur adipisicing elit. In tempora officia possimus fuga, itaque ullam rerum dolorem quo deleniti! Dolores tempore perspiciatis voluptate sed quia sint sunt, porro ipsa qui.</p>
+        <p>Hi, I'm Pavel and I'm an aspiring web developer. I moved to Berlin in August to pursue my passion for web development. Ironhack Berlin was an amazing experience for me and now I'm looking for job opportunities. Outside of programming I enjoy lifting weights, cooking and reading.</p>
         <div className="sc-holder">
         <a rel='noreferrer' target="_blank" href="https://www.linkedin.com/in/j-m-zielinska/"><img className='link-logo' src="https://res.cloudinary.com/dxc2oj7lt/image/upload/v1665059333/sportUp-users-gallery/linkedin_tscs52.png" alt="github" /></a>
         <a rel='noreferrer' target="_blank" href="https://github.com/j-zielinska"><img className='link-logo' src="https://res.cloudinary.com/dxc2oj7lt/image/upload/v1665058862/sportUp-users-gallery/github-logo_kyaxov.png" alt="github" /></a>
         
+        </div>
+      </section>
+      <section className="creator-holder">
+        <h3>Pavel Popov</h3>
+        <img src='https://res.cloudinary.com/dxc2oj7lt/image/upload/v1664887616/sportUp-users-gallery/gxzye5vhjmvkus7kn7mu.jpg' alt="pavel profile" />
+        <p>Hi, I'm Pavel and I'm an aspiring web developer. I moved to Berlin in August to pursue my passion for web development. Ironhack Berlin was an amazing experience for me and now I'm looking for job opportunities. Outside of programming I enjoy lifting weights, cooking and reading.</p>
+        <div className="sc-holder">
+
+        <a rel='noreferrer' target="_blank" href="https://www.linkedin.com/in/pavel-iv-popov/"><img className='link-logo' src="https://res.cloudinary.com/dxc2oj7lt/image/upload/v1665059333/sportUp-users-gallery/linkedin_tscs52.png" alt="github" /></a>
+        <a rel='noreferrer' target="_blank" href="https://github.com/veykos"><img className='link-logo' src="https://res.cloudinary.com/dxc2oj7lt/image/upload/v1665058862/sportUp-users-gallery/github-logo_kyaxov.png" alt="github" /></a>
         </div>
       </section>
     </div>
