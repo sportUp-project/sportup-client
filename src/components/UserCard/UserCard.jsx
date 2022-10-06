247808a4384315ac670c1ddd092cd21f12021491
Pavel Popov <pcveykos@gmail.com>
2022-10-06 10:37:39 +0200

    Add user card changes and adjustments to profile editing

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -5,8 +5,8 @@ export default function UserCard(props) {
   const {user} = props;
 
   return (
-    <div className="user-card">
-      <img src={user.image} alt={user.name} />
+    <div className="user-card" style={{backgroundImage : `url(${user.image})`}}>
+      {/* <img src={user.image} alt={user.name} /> */}
     </div>
   )
 }
