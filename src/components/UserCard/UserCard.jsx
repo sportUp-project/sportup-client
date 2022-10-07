420c18a0e9c8b6ff375f69d54a6fd00bc57588f3
Joanna Zielinska <joannazielinskam@gmail.com>
2022-10-07 10:09:01 +0200

    fixed popup of user card

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -9,7 +9,7 @@ export default function UserCard(props) {
 
 
   return (
-    <div>
+    <div className="user-info">
     <div className="user-card" style={{backgroundImage : `url(${user.image})`}} onMouseEnter={()=>setShowName(true)} onMouseLeave={()=>setShowName(false)}>
        </div>
     {showName && <p className="user-name" >{user.name}</p>}
