76b4ee773b2c031551827b66dd845cdecf22ec39
Joanna Zielinska <joannazielinskam@gmail.com>
2022-10-08 10:41:43 +0200

    about me text adjustemts

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,7 +6,7 @@ export default function AboutPage(props) {
       <section className="creator-holder">
         <h3>Joanna Zielinska</h3>
         <img src="https://res.cloudinary.com/dxc2oj7lt/image/upload/c_scale,q_69,w_1463/v1665062794/sportUp-users-gallery/unnamed_2_axp8td.jpg" alt="joanna profile" />
-        <p>Hi, I'm Joanna! After 6 years of experience as an Architect in an International Office. I followed my passion and became a Web Developer. Currently, I am looking for a job opportunity, to gain experience and expand my knowledge. I enjoy sculpting and painting long-distance cycling and rope climbing in my free time.</p>
+        <p>Hi, I'm Joanna! I come from Poland and after 6 years of experience as a Civil Architect for a Berlin office, I have decided to move to Tech. After a few months of self-learning, and a great Bootcamp (Ironhack), I am searching for a role as a junior Web Developer. I am Berlin-based, an experienced team player, and fluent in English.</p>
         <div className="sc-holder">
         
         <a rel='noreferrer' target="_blank" href="https://www.linkedin.com/in/j-m-zielinska/"><img className='link-logo' src="https://res.cloudinary.com/dxc2oj7lt/image/upload/v1665059333/sportUp-users-gallery/linkedin_tscs52.png" alt="github" /></a>
